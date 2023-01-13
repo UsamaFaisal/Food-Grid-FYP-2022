650689f2fdcde6dfd9573f6783260283fdce8d07
usama <usamashk530@gmail.com>
2023-01-14 00:07:45 +0500

    "Update Welcome page"

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAmVXYAiYFPmKoXDO3jmMsM6xhcXdl3qqk",
     authDomain: "foodgrid-1.firebaseapp.com",
     projectId: "foodgrid-1",
+    
     storageBucket: "foodgrid-1.appspot.com",
     messagingSenderId: "1022672518984",
     appId: "1:1022672518984:web:c7562ba82450d931a9fd7d",
